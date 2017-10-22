d23fb2111a2a348c370622d3a211acd091a681d1
Arturas Molcanovas <a.molcanovas@gmail.com>
2017-10-22 13:40:37 +0100

    try redis-set suite

---
+++ test/redis-set.js
@@ -0,0 +1,21 @@
+const assert = require('assert');
+
+describe('Redis', () => {
+  let redis;
+
+  before('Setting up Redis', () => {
+    redis = require('../server/redis');
+  });
+
+  it('Should have a set function', () => {
+    assert.equal(typeof redis.set, 'function');
+  });
+
+  it('Should have a get function', () => {
+    assert.equal(typeof redis.get, 'function');
+  });
+
+  it('Should have a shouldCache function', () => {
+    assert.equal(typeof redis.shouldCache, 'function');
+  });
+});
