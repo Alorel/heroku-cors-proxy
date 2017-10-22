5a48c9c8a69cc9ae40620521b658881b42d3e189
Arturas Molcanovas <a.molcanovas@gmail.com>
2017-10-22 14:25:12 +0100

    try fix redis url for noenv

--- test/no-redis.js
+++ test/no-redis.js
@@ -1,7 +1,7 @@
 import test from 'ava';
 
 test.before('Removing rediscloud_url env variable', () => {
-  process.env.REDISCLOUD_URL = null;
+  process.env.REDISCLOUD_URL = '';
 });
 
 test('Redis without env', t => {
