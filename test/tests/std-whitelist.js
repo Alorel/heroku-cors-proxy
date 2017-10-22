8e13abfcf424398f0fed3ad50fb7a930d1bdacca
Arturas Molcanovas <a.molcanovas@gmail.com>
2017-10-22 16:26:46 +0100

    whitelist 403 test

--- test/tests/std-whitelist.js
+++ test/tests/std-whitelist.js
@@ -7,6 +7,7 @@ test.before('Set up whitelist', () => {
 });
 
 test('Is array', t => t.true(Array.isArray(whitelist)));
+test('Is frozen', t => t.true(Object.isFrozen(whitelist)));
 test('Has localhost', t => t.true(whitelist.includes('localhost')));
 test('Has 127.0.0.1', t => t.true(whitelist.includes('127.0.0.1')));
 test('Has only 2 items', t => t.is(whitelist.length, 2));
---
+++ test/tests/whitelist-403.js
@@ -0,0 +1,32 @@
+import test from 'ava';
+import request from 'supertest';
+import express from 'express';
+
+let app;
+let req;
+
+test.before('Setup', () => {
+  app = express();
+
+  app.use(require('../../server/filter/req'));
+  app.use(require('../../server/filter/whitelist-403'));
+  app.get('/', (req, res) => res.end(''));
+});
+
+test.cb('Disallowed origin', t => {
+  request(app)
+    .get('/')
+    .set('Origin', 'https://foo.bar')
+    .expect(403, 'Origin foo.bar not allowed')
+    .end(t.end);
+});
+
+for (const allow of ['127.0.0.1', 'localhost']) {
+  test.cb(`Allow ${allow}`, t => {
+    request(app)
+      .get('/')
+      .set('Origin', `https://${allow}`)
+      .expect(200, '')
+      .end(t.end);
+  });
+}
